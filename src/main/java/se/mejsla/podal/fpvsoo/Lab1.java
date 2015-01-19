01f2f8f7b300b5fd293873528987493f62e974f6
Thomas Podal <thomas.podal@gmail.com>
2015-01-20 00:40:29 +0100

    Lab1

---
+++ src/main/java/se/mejsla/podal/fpvsoo/Lab1.java
@@ -0,0 +1,24 @@
+package se.mejsla.podal.fpvsoo;
+
+import se.mejsla.podal.fpvsoo.util.List;
+
+/**
+ * Bra util klass för att göra operationer på en lista.
+ */
+public class Lab1 {
+	private Lab1() {
+	}
+
+	/**
+	 * Tittar ifall ett object finns i en lista.
+	 * 
+	 * @param list
+	 *            listan att söka igenom.
+	 * @param t
+	 *            objectet att söka efter.
+	 * @return returnerar sant ifall objectet t finns i listan
+	 */
+	public static <T> boolean exists(List<T> list, T t) {
+		throw new RuntimeException("Inte riktigt färdig.");
+	}
+}
---
+++ src/test/java/se/mejsla/podal/fpvsoo/testtemplate/Lab1Test.java
@@ -0,0 +1,23 @@
+package se.mejsla.podal.fpvsoo.testtemplate;
+
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static se.mejsla.podal.fpvsoo.util.Lists.list;
+
+import org.junit.Test;
+
+import se.mejsla.podal.fpvsoo.Lab1;
+
+public class Lab1Test {
+
+	@Test
+	public void exists_test1() {
+		assertTrue(Lab1.exists(list("1", "2", "4", "5"), "1"));
+	}
+	@Test
+	public void exists_test2() {
+		assertFalse(Lab1.exists(list("1", "2", "4", "5"), "3"));
+	}
+
+}
