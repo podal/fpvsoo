de2df4e4b7928cedd4e1a9ec0dc919ad0f455ee3
Thomas Podal <thomas.podal@gmail.com>
2015-01-20 00:44:56 +0100

    Lab8

---
+++ src/main/java/se/mejsla/podal/fpvsoo/Lab8.java
@@ -0,0 +1,22 @@
+package se.mejsla.podal.fpvsoo;
+
+import se.mejsla.podal.fpvsoo.util.List;
+
+/**
+ * Bra util klass för att göra operationer på en lista.
+ */
+public class Lab8 {
+	private Lab8() {
+	}
+
+	/**
+	 * Här sorterar vi listan vi skickar in.
+	 * 
+	 * @param list en godtycklig lista
+	 * @return returnerar listan sorterad
+	 */
+	public static <T extends java.lang.Comparable<T>> List<T> sort(List<T> list) {
+		throw new RuntimeException("Inte riktigt färdig.");
+	}
+
+}
