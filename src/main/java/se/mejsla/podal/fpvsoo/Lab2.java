f1498b69fd2ef88539060ec53136958b3955bd11
Thomas Podal <thomas.podal@gmail.com>
2015-01-20 00:41:45 +0100

    Lab2

---
+++ src/main/java/se/mejsla/podal/fpvsoo/Lab2.java
@@ -0,0 +1,26 @@
+package se.mejsla.podal.fpvsoo;
+
+import se.mejsla.podal.fpvsoo.util.List;
+
+/**
+ * Bra util klass för att göra operationer på en lista.
+ */
+public class Lab2 {
+	private Lab2() {
+	}
+
+	/**
+	 * Denna byter ut den alla <code>find</code> med <code>replace</code>.
+	 * 
+	 * @param list
+	 *            listan som behöver bytet berör.
+	 * @param find
+	 *            objectet som ska bytas ut
+	 * @param replace
+	 *            objectet som det ska bytas till
+	 * @return en ny lista där objecten är utbytta
+	 */
+	public static <T> List<T> replace(List<T> list, T find, T replace) {
+		throw new RuntimeException("Inte riktigt färdig.");
+	}
+}
